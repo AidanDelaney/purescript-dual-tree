3d42ef1926eb676c5f774bd4cdf4f8263cca1580
Aidan Delaney <aidan@ontologyengineering.org>
2017-08-10 07:34:38 +1200

    Added wrapping and unwrapping for several of the DUALTree associated types in Test in order to make them an instance of Arbitrary.

--- test/Test.purs
+++ test/Test.purs
@@ -1,17 +1,18 @@
-module Test.Main where
+module Data.Tree.DUAL.Test where
 
 import           Prelude --(class Semigroup, unit, ($), (<$>), (==), append)
 import           Data.Monoid.Action (class Action)
 import           Data.Monoid.Additive
 import           Data.Monoid.Multiplicative
 import           Data.Maybe(Maybe(..), fromMaybe)
+import           Data.Newtype (class Newtype, unwrap, wrap)
 import           Data.Array(singleton)
 import           Data.NonEmpty((:|))
 import           Data.Tuple (Tuple(..))
 import           Data.Semiring(class Semiring)
 import           Control.Apply (lift2)
-import           Data.Tree.DUAL.Internal (empty, leaf, leafU, getU, DUALTree, applyUpre)
-import           Test.QuickCheck.Arbitrary (arbitrary)
+import           Data.Tree.DUAL.Internal (empty, leaf, leafU, getU, DUALTree(..), DUALTreeU(..), DUALTreeNE(..), applyUpre)
+import           Test.QuickCheck.Arbitrary (arbitrary, class Arbitrary)
 import           Test.QuickCheck.Gen (Gen,  oneOf)
 import           Test.QuickCheck (quickCheck)
 
@@ -32,7 +33,7 @@ instance actionD :: Action D Int where
 instance actionDU :: Action D U where
   act (Multiplicative i) (Additive j) = Additive (i * j)
 
-type T = DUALTree D U Boolean Boolean
+newtype DT = DT (DUALTree D U Boolean Boolean)
 
 data DUALTreeExpr d u a l =
     EEmpty
@@ -46,11 +47,39 @@ data DUALTreeExpr d u a l =
 mkU :: Gen U
 mkU = Additive <$> (arbitrary :: Gen Int)
 
+instance arbitraryU :: Arbitrary U where
+  arbitrary = mkU
+
 mkD :: Gen D
 mkD = Multiplicative <$> (arbitrary :: Gen Int)
 
-{-[Sum <$> (arbitrary :: Gen Int)
-                      , ]-}
+instance arbitraryD :: Arbitrary D where
+  arbitrary = mkD
+
+newtype PairUD = PairUD (Tuple U (DUALTreeNE D U Boolean Boolean))
+
+unwrapPairUD :: PairUD -> DUALTreeU D U Boolean Boolean
+unwrapPairUD (PairUD t) = wrap t
+
+instance arbitraryDNE :: Arbitrary PairUD where
+  arbitrary = PairUD <$> (Tuple <$> u <*> t)
+              where
+                u = mkU
+                l = arbitrary :: Gen Boolean
+                t = oneOf $ (LeafU <$> u)  :| singleton (Leaf <$> u <*> l)
+
+newtype DTU = DTU (DUALTreeU D U Boolean Boolean)
+
+instance arbitraryDTU :: Arbitrary DTU where
+  arbitrary = (DTU <<< unwrapPairUD) <$> arbitrary :: Gen PairUD
+
+-- This constant unwrapping sucks.  I don't want to bother making these types instance of
+-- Newtype, but we can't just make an instance of DualTree here as we're not in the same module.
+unwrapDTU :: forall d u a l. DTU -> Maybe (DUALTreeU D U Boolean Boolean)
+unwrapDTU (DTU d) = Just d
+
+instance arbitraryDT :: Arbitrary DT where
+  arbitrary = DT <<< wrap <<< unwrapDTU <$> arbitrary :: Gen DTU
 
 mkLeaf :: forall d a. Gen (DUALTreeExpr d U a Int)
 mkLeaf = oneOf $ l :| singleton lu
@@ -80,8 +109,8 @@ prop_leaf_u u = getU (leaf u unit) == Just u
 prop_leafU_u :: U -> Boolean
 prop_leafU_u u = getU (leafU u) == Just u
 
-prop_applyUpre :: forall d a l. U -> T -> Boolean
-prop_applyUpre u t = getU (applyUpre u t) == Just u -- Just (u `append` fromMaybe empty (getU t))
+prop_applyUpre :: forall d a l. U -> DT -> Boolean
+prop_applyUpre u (DT t) = getU (applyUpre u t) == Just u -- Just (u `append` fromMaybe empty (getU t))
 
 --prop_applyUpost :: U -> T -> Boolean
 --prop_applyUpost u t = getU (applyUpost u t) == Just (fromMaybe mempty (getU t) `append` u)
@@ -89,3 +118,5 @@ prop_applyUpre u t = getU (applyUpre u t) == Just u -- Just (u `append` fromMayb
 
 main = do
   quickCheck prop_leaf_u
+  quickCheck prop_leafU_u
+  quickCheck prop_applyUpre
