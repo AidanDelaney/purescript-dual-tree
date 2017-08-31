99bf410bf5fcf9079c00df18fed2e56f6b8c2b44
Aidan Delaney <aidan@ontologyengineering.org>
2017-09-01 07:57:20 +1200

    buildTree can now deal with the Concat case.

--- test/Test.purs
+++ test/Test.purs
@@ -6,14 +6,15 @@ import           Data.Monoid.Additive
 import           Data.Monoid.Multiplicative
 import           Data.Maybe(Maybe(..), fromMaybe)
 import           Data.Newtype (class Newtype, unwrap, wrap)
-import           Data.Array(singleton)
-import           Data.NonEmpty((:|))
+import           Data.List.Lazy (replicate)
+import           Data.List.NonEmpty (NonEmptyList(..), singleton, fromList)
+import           Data.NonEmpty(NonEmpty(..), (:|), foldMap1)
 import           Data.Tuple (Tuple(..))
 import           Data.Semiring(class Semiring)
 import           Control.Apply (lift2)
 import           Data.Tree.DUAL.Internal (empty, leaf, leafU, getU, DUALTree(..), DUALTreeU(..), DUALTreeNE(..), applyUpre)
 import           Test.QuickCheck.Arbitrary (arbitrary, class Arbitrary)
-import           Test.QuickCheck.Gen (Gen,  oneOf)
+import           Test.QuickCheck.Gen (Gen, chooseInt, oneOf, listOf)
 import           Test.QuickCheck (quickCheck)
 
 newtype U = Additive Int
@@ -39,7 +40,7 @@ data DUALTreeExpr d u a l =
     EEmpty
   | ELeaf u l
   | ELeafU u
---  | EConcat (NonEmpty (DUALTreeExpr d u a l))
+  | EConcat (NonEmptyList (DUALTreeExpr d u a l))
 --  | EAct d (DUALTreeExpr d u a l)
 --  | EAnnot a (DUALTreeExpr d u a l)
 --  deriving (Show, Typeable)
@@ -66,7 +67,7 @@ instance arbitraryDNE :: Arbitrary PairUD where
               where
                 u = mkU
                 l = arbitrary :: Gen Boolean
-                t = oneOf $ (LeafU <$> u)  :| singleton (Leaf <$> u <*> l)
+                t = oneOf $ NonEmpty (LeafU <$> u) [(LeafU <$> u), (Leaf <$> u <*> l)]
 
 newtype DTU = DTU (DUALTreeU D U Boolean Boolean)
 
@@ -82,20 +83,25 @@ instance arbitraryDT :: Arbitrary DT where
   arbitrary = DT <<< wrap <<< unwrapDTU <$> arbitrary :: Gen DTU
 
 mkLeaf :: forall d a. Gen (DUALTreeExpr d U a Int)
-mkLeaf = oneOf $ l :| singleton lu
+mkLeaf = oneOf $ NonEmpty l [l, lu]
   where
     l = (lift2 ELeaf mkU (arbitrary :: Gen Int))
     lu = (ELeafU <$> mkU)
 
 mkTreeExpr :: forall d a. Int -> Gen (DUALTreeExpr d U a Int)
 mkTreeExpr 0 = mkLeaf
-mkTreeExpr n = mkLeaf
+mkTreeExpr n = do
+                 len <- chooseInt 1 n
+                 ls <- fromList <$> listOf len mkLeaf -- (mkTreeExpr (len-1))
+                 case ls of
+                   (Just xs) -> pure $ EConcat xs
+                   Nothing -> mkLeaf
 
-buildTree :: forall d u a l. Semigroup d => Semigroup u => DUALTreeExpr d U a l -> DUALTree d U a l
+buildTree :: forall d a l. Semigroup d => DUALTreeExpr d U a l -> DUALTree d U a l
 buildTree EEmpty       = empty
 buildTree (ELeaf u l)  = leaf u l
 buildTree (ELeafU u)   = leafU u
---buildTree (EConcat ts) = sconcat (NEL.map buildTree ts)
+buildTree (EConcat (NonEmptyList ts)) =  foldMap1 buildTree ts
 --buildTree (EAct d t)   = applyD d (buildTree t)
 --buildTree (EAnnot a t) = annot a (buildTree t)
 
@@ -109,7 +115,7 @@ prop_leaf_u u = getU (leaf u unit) == Just u
 prop_leafU_u :: U -> Boolean
 prop_leafU_u u = getU (leafU u) == Just u
 
-prop_applyUpre :: forall d a l. U -> DT -> Boolean
+prop_applyUpre :: U -> DT -> Boolean
 prop_applyUpre u (DT t) = getU (applyUpre u t) == Just u -- Just (u `append` fromMaybe empty (getU t))
 
 --prop_applyUpost :: U -> T -> Boolean
